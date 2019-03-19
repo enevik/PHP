47f3db494172fc2425055e89f2d15dfe02a783d6
Unknown <batinsimsek1998@gmail.com>
2019-03-19 15:37:04 +0100

    inport

--- index.php
+++ index.php
@@ -1,5 +1,67 @@
 <?php require 'header.php'; ?>
 
+    <!doctype html>
+    <html class="no-js" lang="">
+
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="x-ua-compatible" content="ie=edge">
+        <title></title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+        <link rel="manifest" href="site.webmanifest">
+        <link rel="apple-touch-icon" href="icon.png">
+        <!-- Place favicon.ico in the root directory -->
+
+        <link rel="stylesheet" href="css/normalize.css">
+        <link rel="stylesheet" href="css/main.css">
+    </head>
+
+    <body>
+    <!--[if lte IE 9]>
+    <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="https://browsehappy.com/">upgrade your browser</a> to improve your experience and security.</p>
+    <![endif]-->
+
+    <!-- Add your site or application content here -->
+
+    <main>
+        <div class="container">
+            <h1>De Gokker</h1>
+
+            <form action="/action_page.php">
+                <div class="imgcontainer">
+                    <img src="img/banner.png" alt="Avatar" class="avatar">
+                </div>
+
+                <div class="info-degokker">
+                    <h2>Welkom bij onze website!</h2>
+                    <p>Hier ken je ons game de gokker vinden. We hopen dat je de game leuk ga vinden! De game gaat
+                        over een honderace. Je kan alleen spelen of met vrienden!
+                    </p>
+                </div>
+
+                <button type="submit">Download File</button>
+
+        </div>
+    </main>
+    <script src="js/vendor/modernizr-3.6.0.min.js"></script>
+    <script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
+    <script>window.jQuery || document.write('<script src="js/vendor/jquery-3.3.1.min.js"><\/script>')</script>
+    <script src="js/plugins.js"></script>
+    <script src="js/main.js"></script>
+
+    <!-- Google Analytics: change UA-XXXXX-Y to be your site's ID. -->
+    <script>
+        window.ga = function () { ga.q.push(arguments) }; ga.q = []; ga.l = +new Date;
+        ga('create', 'UA-XXXXX-Y', 'auto'); ga('send', 'pageview')
+    </script>
+    <script src="https://www.google-analytics.com/analytics.js" async defer></script>
+    </body>
+
+    </html>
+
+
    <h1>Welcome to AMO Login system Home Page! </h1>
    <p>Nothing special to see here but that's not the point.</p>
    <p>Try going to the ultra secret ultimate vip <a href='admin.php'> admin </a> page without logging in first</p>
