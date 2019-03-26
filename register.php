d47ab8783545cfc6c7080c261c77226a11d9087e
Unknown <enevik@hotmail.com>
2019-03-26 11:24:21 +0100

    register bug gefixt

--- register.php
+++ register.php
@@ -2,14 +2,6 @@
 
 require 'config.php';
 
-if(!filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
-    exit('Invalid email address'); // Use your own error handling ;)
-}
-$select = mysqli_query($connectionID, "SELECT `email` FROM `game` WHERE `email` = '".$_POST['email']."'") or exit(mysqli_error($connectionID));
-if(mysqli_num_rows($select)) {
-    exit('This email is already being used');
-}
-
 ?>
 <!doctype html>
 <html class="no-js" lang="">
